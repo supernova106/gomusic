e4b5283f6bb7ec6b8cb60d8d76882115843e9381
Bean <binhnguyenntu09@gmail.com>
2015-11-21 22:31:23 +0800

    update with emojione

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var gomusic  = require('express')();
 var http = require('http').Server(gomusic);
 var io = require('socket.io')(http);
-var people = {};
+var clients = [];
 
 gomusic.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -9,12 +9,37 @@ gomusic.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	console.log('a user connected');
-	socket.on('chat message', function(msg){
-    		io.emit('chat message', msg);
- 	 });
+	socket.on('new user', function (data, callback){
+		if (clients.indexOf(data) != -1) {
+			callback(false);
+		}
+		else {
+			callback(true);
+			socket.nickName = data;
+			clients.push(socket.nickName);
+            updateNickName();
+		}
+	});
+
+    socket.on('disconect', function(data) {
+        if (!socket.nickName) {
+            return;
+        }
+        clients.splice(clients.indexOf(socket.nickName), 1);
+        updateNickName();
+
+    });
+
+	socket.on('chat message', function(data){
+		io.emit('chat message', {msg: data, username: socket.nickName});
+	});
 
 });
 
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
+
+function updateNickName() {
+    io.emit('usernames', clients);
+}
